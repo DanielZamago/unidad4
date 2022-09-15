23f50423fcc7b68740abc2faee5da28b8da73cc2
Bilobatetonto <cris_misterio10@hotmail.com>
2022-09-15 10:01:34 -0600

    cambio de lugar del index

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceso al panel</title>
-    <link rel="stylesheet" href="public/css/main.css">
+    
+    <link rel="stylesheet" type="text/css" href="public/css/main.css">
+
 </head>
 <body>
     <div class="container">
